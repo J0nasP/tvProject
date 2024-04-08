f2508f37227ace4ae429530b168c10ba59608d91
Jonas <petersen0108@gmail.com>
2024-04-08 08:10:49 +0200

    Finnished with the migration file, only needs some relations added

--- tableBooking/database/migrations/2024_04_05_104445_customer.php
+++ tableBooking/database/migrations/2024_04_05_104445_customer.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Validation\Rules\Unique;
 
 return new class extends Migration
 {
@@ -17,8 +18,42 @@ return new class extends Migration
             $table->string('Lastname');
             $table->string('street');
             $table->int('zipcode');
-            $table->string('email');
+            $table->string('email')->unique();
             $table->int('phoneNumber');
+            $table->int('visitsTotal');
+        });
+
+        Schema::create(('customerLogin'), function(Blueprint $table){
+            $table->id();
+            $table->string('email')->primary();
+            $table->timestamp('emailVerifiedAt')->nullable();
+            $table->string('password');
+        });
+
+        Schema::create('restaurant', function(Blueprint $table){
+            $table->id();
+            $table->string('name');
+            $table->string('street');
+            $table->string('city');
+            $table->int('zipCode');
+            $table->int('phoneNumber');
+        });
+
+        Schema::create('restaurantLogin', function(Blueprint $table){
+            $table->id();
+            $table->string('email')->primary();
+            $table->timestamp('emailVerifiedAt')->nullable();
+            $table->string('password');
+        });
+
+        Schema::create('reservation', function(Blueprint $table){
+            $table->id();
+            $table->int('pax');
+            $table->date('date');
+            $table->string('timeOfDay');
+            $table->string('customer');
+            $table->string('remarks');
+            $table->int('visitsTotal');
         });
     }
 
@@ -27,6 +62,10 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        Schema::dropIfExists('customer');
+        Schema::dropIfExists('customerLogin');
+        Schema::dropIfExists('restaurant');
+        Schema::dropIfExists('restaurantLogin');
+        Schema::dropIfExists('reservations');
     }
 };
