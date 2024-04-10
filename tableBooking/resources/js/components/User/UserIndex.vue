318512003c2f6402e6b6d67a9cf496abb1e17b63
Jonas <petersen0108@gmail.com>
2024-04-10 13:29:22 +0200

    init off vue

---
+++ tableBooking/resources/js/components/User/UserIndex.vue
@@ -0,0 +1,33 @@
+<template>
+    <table class="table table-hover">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">name</th>
+                <th scope="col">Email</th>
+                <th scope="col">Updated</th>
+                <th scope="col">Created at</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr 
+                v-for="user in users"
+                :key="user.id"
+            > 
+
+            </tr>
+        </tbody>
+    </table>
+</template>
+<script>
+    export default {
+        data() {
+            return {
+                users: []
+            }
+        },
+        created() {
+            // TODO AJAX request
+        },
+    }
+</script>
