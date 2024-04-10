27ba1f3b6ca9aa9fff96ba3806cbb44930080043
Jonas <petersen0108@gmail.com>
2024-04-10 07:46:14 +0200

    installed vue for login table

--- tableBooking/resources/views/welcome.blade.php
+++ tableBooking/resources/views/welcome.blade.php
@@ -4,8 +4,9 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
-
+        <title>{{ config('app.name', Laravel)}}</title>
+        <!-- Script -->
+        @vite(['resources/css/app.css', 'resourcess/js/app.js'])
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
