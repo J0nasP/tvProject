dfcbaeeb95fa0efbfbf972dd4b51bdd9c8154639
Jonas <petersen0108@gmail.com>
2024-04-17 13:18:38 +0200

    sdihsdæfklghsædlghy

---
+++ booking_application/lib/calendar.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:booking_calendar/booking_calendar.dart';
+import 'package:intl/date_symbol_data_local.dart';
+
+void main() {
+  initializeDateFormatting()
+      .then((_) => runApp(const BookingCalendarDemoApp()));
+}
+
+class BookingCalendarDemoApp extends StatefulWidget {
+  const BookingCalendarDemoApp({Key? key}) : super(key: key);
+
+  @override
+  State<BookingCalendarDemoApp> createState() => _BookingCalendarDemoAppState();
+}
+
+class _BookingCalendarDemoAppState extends State<BookingCalendarDemoApp> {
+  final now = DateTime.now();
+  late BookingService mockBookingService;
+
+  @override
+  void initState() {
+    super.initState();
+    // DateTime.now().startOfDay
+    // DateTime.now().endOfDay
+    mockBookingService = BookingService(
+        serviceName: 'Booking Service',
+        serviceDuration: 30,
+        bookingEnd: DateTime(now.year, now.month, now.day, 21, 30),
+        bookingStart: DateTime(now.year, now.month, now.day, 11, 0));
+  }
+
+  Stream<dynamic>? getBookingStreamMock(
+      {required DateTime end, required DateTime start}) {
+    return Stream.value([]);
+  }
+
+  Future<dynamic> uploadBookingMock(
+      {required BookingService newBooking}) async {
+    await Future.delayed(const Duration(seconds: 1));
+    converted.add(DateTimeRange(
+        start: newBooking.bookingStart, end: newBooking.bookingEnd));
+    print('${newBooking.toJson()} has been uploaded');
+  }
+
+  List<DateTimeRange> converted = [];
+
+  List<DateTimeRange> convertStreamResultMock({required dynamic streamResult}) {
+    ///here you can parse the streamresult and convert to [List<DateTimeRange>]
+    ///take care this is only mock, so if you add today as disabledDays it will still be visible on the first load
+    ///disabledDays will properly work with real data
+    DateTime first = now;
+
+    converted.add(
+        DateTimeRange(start: first, end: now.add(const Duration(minutes: 30))));
+
+    return converted;
+  }
+
+  List<DateTimeRange> generatePauseSlots() {
+    return [
+      DateTimeRange(
+          start: DateTime(now.year, now.month, now.day, 16, 0),
+          end: DateTime(now.year, now.month, now.day, 16, 30))
+    ];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Booking Calendar Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Booking Calendar Demo'),
+        ),
+        body: Center(
+          child: BookingCalendar(
+            bookingGridChildAspectRatio: 4,
+            bookingService: mockBookingService,
+            convertStreamResultToDateTimeRanges: convertStreamResultMock,
+            getBookingStream: getBookingStreamMock,
+            uploadBooking: uploadBookingMock,
+            pauseSlots: generatePauseSlots(),
+            hideBreakTime: true,
+            loadingWidget: const Text('Fetching data...'),
+            uploadingWidget: const CircularProgressIndicator(),
+            locale: 'da_dk',
+            startingDayOfWeek: StartingDayOfWeek.monday,
+            wholeDayIsBookedWidget:
+                const Text('Sorry, for this day everything is booked'),
+            disabledDays: const [7],
+          ),
+        ),
+      ),
+    );
+  }
+}
