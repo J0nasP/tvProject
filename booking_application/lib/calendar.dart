b8ad18b181530a7a86334f19b8341815a02bc520
Jonas <petersen0108@gmail.com>
2024-04-29 13:04:00 +0200

    added locale and started on time picker

--- booking_application/lib/calendar.dart
+++ booking_application/lib/calendar.dart
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
+import 'package:intl/date_symbol_data_local.dart';
 import 'package:table_calendar/table_calendar.dart';
 import 'shared/utils.dart';
 
